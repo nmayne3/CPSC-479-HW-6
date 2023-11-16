cf2f0c006b2620aa3a868eb5a278ca0760cb3b36
nmayne3 <nmayne3@gmail.com>
2023-11-16 03:41:43 -0800

    Added Comments

--- main.cu
+++ main.cu
@@ -11,19 +11,17 @@ __host__ void print_matrix(int *matrix, int N) {
 	}
 }
 
-// Populates matrix with random values between 0 -- 10
+// Populates NxN matrix with random values between 0 - 9
 __host__ void populate(int* matrix, int N) {
 	srand(time(NULL));
-	int count = 0;
 	for(int i = 0; i < N; ++i) {
 		for(int j = 0; j < N; j++) {
 			matrix[i*N + j] = rand() % 10; 
-			if(matrix[i*N + j] % 2 == 0)
-				count++;
 		}
 	}
 }
 
+// Exercise 1: Counts number of even values in NxN matrix
 __global__ void count(int *matrix, int *count, int N) {
 	unsigned id = blockIdx.x * blockDim.x + threadIdx.x;
 	if(id < N*N) {
@@ -33,22 +31,20 @@ __global__ void count(int *matrix, int *count, int N) {
 	}
 }
 
+// Exercise 2: Squares NxN matrix
 __global__ void square(int *matrix, int *result, int N) {
 	unsigned id = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned i = id / N;
 	unsigned j = id % N;
 	for (unsigned k = 0; k < N; ++k) {
-		result[i*N + j] += matrix[i*N + k] *
-			matrix[k*N + j];
+		result[i*N + j] += matrix[i*N + k] * matrix[k*N + j];
 	}
 }
 
 
 int main() {
-	// Exercise 1
-	int N = 16;
-
 	// Create matrices of size N
+	int N = 16;
 	int *h_inmatrix = (int*)malloc(N*N*sizeof(int));
 	int *hcount = (int*)malloc(sizeof(int));
 	int *d_inmatrix;
@@ -62,12 +58,14 @@ int main() {
 	cudaMemcpy(d_inmatrix, h_inmatrix, N*N*sizeof(int), cudaMemcpyHostToDevice);
 	print_matrix(h_inmatrix, N);
 
+	// Calculate number of even values
 	*hcount = 0;
 	cudaMemcpy(dcount, hcount, sizeof(int), cudaMemcpyHostToDevice);
 	count<<<N, N>>>(d_inmatrix, dcount, N);
 	cudaMemcpy(hcount, dcount, sizeof(int), cudaMemcpyDeviceToHost);
 	printf("Number of Even values: %d\n\n", *hcount);
 
+	// Square Matrix 
 	square<<<N, N>>>(d_inmatrix, d_outmatrix, N);
 	cudaMemcpy(h_inmatrix, d_outmatrix, N*N*sizeof(int), cudaMemcpyDeviceToHost);
 	print_matrix(h_inmatrix, N);
